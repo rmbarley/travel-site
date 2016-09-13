ad5e576a113113f9a4e27f86ba87cadf08f85305
Ryan <rmbarley@gmail.com>
2016-09-13 10:40:34 -0400

    Styled header for large screens

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -4,11 +4,12 @@
       cssImport = require('postcss-import'),
       autoprefixer = require('autoprefixer'),
       mixins = require('postcss-mixins'),
+      hexrgba = require('postcss-hexrgba'),
       cssvars = require('postcss-simple-vars');
 
     gulp.task('styles', function() {
       return gulp.src('./app/assets/styles/styles.css')
-        .pipe(postcss([cssImport, mixins, cssvars, nested, autoprefixer]))
+        .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
         .on('error', function(errorInfo) {
         	console.log(errorInfo.toString());
         	this.emit('end');
